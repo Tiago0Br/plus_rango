635be83dfb28347e358c8a50cdfe4d216354f118
Tiago Lopes <tiagoltavares2002@gmail.com>
2024-04-30 20:06:52 -0300

    feat: exibindo listagem de categorias
    - Foi criado o componente de lista de categorias
    - Foi criado o componente de categoria
    - Foi adicionado o link da imagem no arquivo de configuração do Next
    - Foi alterada a cor do botão

--- app/_components/category-list.tsx
+++ app/_components/category-list.tsx
@@ -1,3 +1,14 @@
-export const CategoryList = () => {
-  return <div className="flex gap-1"></div>;
+import { db } from "../_lib/prisma";
+import { CategoryItem } from "./category-item";
+
+export const CategoryList = async () => {
+  const categories = await db.category.findMany({});
+
+  return (
+    <div className="grid grid-cols-2 gap-3">
+      {categories.map((category) => (
+        <CategoryItem key={category.id} category={category} />
+      ))}
+    </div>
+  );
 };
