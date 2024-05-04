f638255887eaa05580324a322deb04ae5176e12a
Tiago Lopes <tiagoltavares2002@gmail.com>
2024-05-03 21:49:52 -0300

    feat: criação do componente de busca de restaurantes
    - Foi criada a página com o resultado da busca
    - Foi criada uma server action para busca dos restaurantes com a query informada

--- app/_components/search.tsx
+++ app/_components/search.tsx
@@ -1,12 +1,31 @@
+"use client";
+
 import { SearchIcon } from "lucide-react";
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 export const Search = () => {
+  const router = useRouter();
+  const [search, setSearch] = useState("");
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
+
+  const handleSearchSubmit = () => {
+    if (search.trim() !== "") router.push(`/restaurants/?search=${search}`);
+    setSearch("");
+  };
   return (
     <div className="flex gap-2">
-      <Input placeholder="Buscar restaurantes" className="border-none" />
-      <Button size="icon">
+      <Input
+        placeholder="Buscar restaurantes"
+        className="border-none"
+        onChange={handleChange}
+      />
+      <Button size="icon" onClick={handleSearchSubmit}>
         <SearchIcon size={20} />
       </Button>
     </div>
